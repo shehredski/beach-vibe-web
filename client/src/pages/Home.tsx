6e5a26e9a371d61bc3f31c7cf980ff10f7ffcbde
shehredski <118177738+shehredski@users.noreply.github.com>
2026-04-20 22:37:11 +0300

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import Navigation from "@/components/Navigation";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
-// import Menu from "@/components/Menu"; // <--- ТОВА Е ИЗТРИТО
 import Gallery from "@/components/Gallery";
 import Events from "@/components/Events";
 import Promotions from "@/components/Promotions";
@@ -15,7 +14,6 @@ export default function Home() {
       <Navigation />
       <Hero />
       <About />
-      {/* <Menu /> - Вече не е тук, за да не тежи на началната страница */}
       <Gallery />
       <Events />
       <Promotions />
