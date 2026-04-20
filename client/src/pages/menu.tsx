199f2b0c6ddc6be33cfead60ff08d0fd1da7ca4f
shehredski <118177738+shehredski@users.noreply.github.com>
2026-04-20 23:10:13 +0300

    Update menu.tsx

--- client/src/pages/menu.tsx
+++ client/src/pages/menu.tsx
@@ -1,12 +1,12 @@
+import Navigation from "@/components/Navigation"; // 1. Поправено име
 import Menu from "@/components/Menu"; 
-import { Navbar } from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
 export default function MenuPage() {
   return (
-    <div className="min-h-screen">
-      <Navbar />
-      <main className="py-20">
+    <div className="min-h-screen bg-background">
+      <Navigation /> {/* 2. Поправено име */}
+      <main className="pt-10">
         <Menu />
       </main>
       <Footer />
