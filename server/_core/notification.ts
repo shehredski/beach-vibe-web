29acecee09420d353e829bc41b9ae951ee52cc24
shehredski <118177738+shehredski@users.noreply.github.com>
2026-04-20 18:06:43 +0300

    Update notification.ts

--- server/_core/notification.ts
+++ server/_core/notification.ts
@@ -68,19 +68,19 @@ export async function notifyOwner(
 ): Promise<boolean> {
   const { title, content } = validatePayload(payload);
 
-  if (!ENV.forgeApiUrl) {
-    throw new TRPCError({
-      code: "INTERNAL_SERVER_ERROR",
-      message: "Notification service URL is not configured.",
-    });
-  }
+  //if (!ENV.forgeApiUrl) {
+    //throw new TRPCError({
+      //code: "INTERNAL_SERVER_ERROR",
+      //message: "Notification service URL is not configured.",
+    //});
+  //}
 
-  if (!ENV.forgeApiKey) {
-    throw new TRPCError({
-      code: "INTERNAL_SERVER_ERROR",
-      message: "Notification service API key is not configured.",
-    });
-  }
+  //if (!ENV.forgeApiKey) {
+    //throw new TRPCError({
+      //code: "INTERNAL_SERVER_ERROR",
+      //message: "Notification service API key is not configured.",
+    //});
+  //}
 
   const endpoint = buildEndpointUrl(ENV.forgeApiUrl);
 
