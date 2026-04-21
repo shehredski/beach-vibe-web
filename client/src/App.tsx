5552c5accb576be010e318064716f5fa92b72009
shehredski <118177738+shehredski@users.noreply.github.com>
2026-04-21 16:31:08 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import EventsPage from "@/components/EventsPage";
 import { Route, Switch } from "wouter";
-import Home from "./pages/Home";
+import Home from "./components/Home"; // СМЕНЕНО: Използваме файла в components, който редактирахме
 import MenuPage from "./pages/MenuPage";
 import NotFound from "./pages/NotFound";
 import { ThemeProvider } from "./contexts/ThemeContext";
@@ -11,10 +11,19 @@ import ErrorBoundary from "./components/ErrorBoundary";
 function Router() {
   return (
     <Switch>
+      {/* Начална страница - вече без секцията Events */}
       <Route path="/" component={Home} />
+      
+      {/* Страница с менюто */}
       <Route path="/menu" component={MenuPage} />
+      
+      {/* Новата страница за събития */}
+      <Route path="/events" component={EventsPage} /> 
+      
+      {/* ВАЖНО: NotFound трябва да е ВИНАГИ най-отдолу. 
+          Ако е над /events, той ще "улови" заявката и ще покаже 404.
+      */}
       <Route component={NotFound} />
-      <Route path="/events" component={EventsPage} />
     </Switch>
   );
 }
