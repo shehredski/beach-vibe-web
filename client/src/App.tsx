810371072318032ba9c12defe3ce9a417bf9d2f3
shehredski <118177738+shehredski@users.noreply.github.com>
2026-04-24 14:34:22 +0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import React, { createContext, useState, useContext } from "react";
 import EventsPage from "@/components/EventsPage";
 import { Route, Switch } from "wouter";
 import Home from "@/components/Home";
@@ -7,7 +8,14 @@ import { ThemeProvider } from "./contexts/ThemeContext";
 import { TooltipProvider } from "./components/ui/tooltip";
 import { Toaster } from "@/components/ui/sonner";
 import ErrorBoundary from "./components/ErrorBoundary";
-import AdminEvents from "./pages/AdminEvents"; // 1. Импортът е тук
+import AdminEvents from "./pages/AdminEvents";
+
+// 1. Създаваме контекст за езика, за да е достъпен навсякъде
+export type Language = 'bg' | 'en';
+export const LanguageContext = createContext<{
+  lang: Language;
+  setLang: (l: Language) => void;
+}>({ lang: 'bg', setLang: () => {} });
 
 function Router() {
   return (
@@ -15,24 +23,30 @@ function Router() {
       <Route path="/" component={Home} />
       <Route path="/menu" component={MenuPage} />
       <Route path="/events" component={EventsPage} /> 
-      
-      {/* 2. ТОВА Е КРИТИЧНОТО МЯСТО: */}
       <Route path="/admin-events" component={AdminEvents} />
-      
       <Route component={NotFound} />
     </Switch>
   );
 }
 
 export default function App() {
+  // 2. Стейт за езика - по подразбиране е български
+  const [lang, setLang] = useState<Language>('bg');
+
   return (
     <ErrorBoundary>
-      <ThemeProvider defaultTheme="light">
-        <TooltipProvider>
-          <Toaster />
-          <Router />
-        </TooltipProvider>
-      </ThemeProvider>
+      {/* 3. Опаковаме всичко в LanguageContext.Provider */}
+      <LanguageContext.Provider value={{ lang, setLang }}>
+        <ThemeProvider defaultTheme="light">
+          <TooltipProvider>
+            <Toaster />
+            <Router />
+          </TooltipProvider>
+        </ThemeProvider>
+      </LanguageContext.Provider>
     </ErrorBoundary>
   );
 }
+
+// 4. Помощна кука, която ще ползваш в Home, About и Navbar
+export const useLanguage = () => useContext(LanguageContext);
